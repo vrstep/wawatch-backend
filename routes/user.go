462f552ffa283aecd7f18ab01141b6d199121e2b
Amir <amir.batyrggaliev@gmail.com>
2025-04-09 20:46:37 +0500

    Fix user route url

--- routes/user.go
+++ routes/user.go
@@ -3,7 +3,7 @@ package routes
 import "github.com/gin-gonic/gin"
 
 func UserRoute(router *gin.Engine) {
-	router.GET("/user", func(c *gin.Context) {
+	router.GET("/", func(c *gin.Context) {
 		c.String(200, "User route")
 	})
 }
