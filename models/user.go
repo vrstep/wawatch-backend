e1437a8a45fead98592ced455c40f7a0e94592cb
Amir <amir.batyrggaliev@gmail.com>
2025-04-10 08:23:19 +0500

    Remove redundant id

--- models/user.go
+++ models/user.go
@@ -4,7 +4,6 @@ import "gorm.io/gorm"
 
 type User struct {
 	gorm.Model
-	Id       int    `json:"id" gorm:"primaryKey"`
 	Username string `json:"username" gorm:"unique;not null"`
 	Password string `json:"password" gorm:"not null"`
 	Email    string `json:"email" gorm:"unique;not null"`
