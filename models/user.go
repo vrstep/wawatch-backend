b98ba3dea732a66bc7d2fa54cfa6c0f90091f269
Amir <amir.batyrggaliev@gmail.com>
2025-04-16 22:05:07 +0500

    Update user model

--- models/user.go
+++ models/user.go
@@ -4,7 +4,9 @@ import "gorm.io/gorm"
 
 type User struct {
 	gorm.Model
-	Username string `json:"username" gorm:"unique;not null"`
-	Password string `json:"password" gorm:"not null"`
-	Email    string `json:"email" gorm:"unique;not null"`
+	Username       string `json:"username" gorm:"unique;not null"`
+	Password       string `json:"password" gorm:"not null"`
+	Email          string `json:"email" gorm:"unique;not null"`
+	Role           string `json:"role" gorm:"not null"`
+	ProfilePicture string `json:"profile_picture" gorm:"default:'default.jpg'"`
 }
